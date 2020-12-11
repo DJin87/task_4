1a0b1e2f8ecc1622e36d3a4c195deba913e37048
DJin87 <jins@inbox.lv>
2020-12-11 22:26:52 +0200

    4/1

---
+++ task_4/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  task_4
+//
+//  Created by DJin on 11/12/2020.
+//
+
+import Foundation
+
+
+
+
--- task_4/ViewController.swift
+++ task_4/ViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var passwordName: UITextField!
+    @IBOutlet var logInButton: UIButton!
+    
+    private let userNamePass = "User"
+    private let passPass = "password"
+    
+    // MARK: Navigation
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            guard segue.identifier = "logIn" else { return }
+            let secondView = segue.identifier as! SecondViewController
+            secondView.userNamePass = userName.text
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
